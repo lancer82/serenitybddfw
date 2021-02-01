f56895843f14e3ce130290f6b9daef159e9fcbca
lancer82 <lp_job@163.com>
2021-02-01 23:02:12 +0800

    update all project

--- src/test/java/net/serenity/bdd/pages/SogouPage.java
+++ src/test/java/net/serenity/bdd/pages/SogouPage.java
@@ -2,11 +2,16 @@ package net.serenity.bdd.pages;
 
 import net.serenity.bdd.util.ConfUtil;
 import net.thucydides.core.pages.PageObject;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchWindowException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.io.IOException;
+import java.net.URI;
 import java.util.Collections;
 import java.util.Set;
 
@@ -39,6 +44,30 @@ public class SogouPage extends PageObject {
             }
         }
         //driver.switchTo().window(parentWindowHandle);
+        driver.quit();
+    }
+
+    @Test
+    public void TestSelenoid() throws IOException, InterruptedException {
+        //使用remote driver
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("firefox");
+        capabilities.setVersion("84.0");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        RemoteWebDriver driver = new RemoteWebDriver(
+                URI.create("http://localhost:4444/wd/hub").toURL(),
+                capabilities
+        );
+
+        driver.get("http://www.baidu.com");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.id("kw")).sendKeys("豆瓣");
+        driver.findElement(By.id("su")).click();
+        System.out.println(driver.getTitle());
+        Thread.sleep(20000);
+        driver.getWindowHandle();
         driver.quit();
     }
 }
